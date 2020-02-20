87503ca04168a4776d8337f1f09bf52ed44a181e
Nick Cody <nicholas.james.cody@gmail.com>
2020-02-20 18:36:00 -0500

    Test Commit

---
+++ TestingAssignment/src/TestMain.java
@@ -0,0 +1,20 @@
+/**
+ * @author Group B; Nick Cody, Ben Hestad, Jaydeon Jacobs
+ * 
+ * @section CSCI 330
+ * 
+ * @date 2/12/2019
+ * 
+ * @assignment Implementation Assignment I
+ * 
+ * @description This program demonstrates a unit test of the addInstruction method of the Recipe class
+ */
+public class TestMain {
+
+	public static void main(String[] args) {
+
+		
+		UnitTest.testAddInstruction();
+	}
+
+}
